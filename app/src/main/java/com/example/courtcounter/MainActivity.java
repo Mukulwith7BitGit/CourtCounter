659a6fd62fe005a330e2d99d88659b841344f4b2
Mukul Dalal <mukuldalalnfs@gmail.com>
2022-03-10 17:53:29 +0530

    Second commit. Added all functionalities and designs. Disabled autorotate. Final App.

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA=0;
+    int scoreTeamB=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
     }
+
     public void addThreeForTeamA(View v){
         scoreTeamA+=3;
         displayForTeamA(scoreTeamA);
@@ -32,5 +37,23 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamA(View v){
         scoreTeamA+=1;
         displayForTeamA(scoreTeamA);
+    }
+    public void addThreeForTeamB(View v){
+        scoreTeamB+=3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addTwoForTeamB(View v){
+        scoreTeamB+=2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void addOneForTeamB(View v){
+        scoreTeamB+=1;
+        displayForTeamB(scoreTeamB);
+    }
+    public void resetScores(View v){
+        scoreTeamA=0;
+        scoreTeamB=0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
